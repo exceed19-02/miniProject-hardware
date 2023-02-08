b3749025ee0f4bf0b357413ce7a2f83bfd6b3d6b
VeryHardBit <yanothaic15@gmail.com>
2023-02-08 14:03:35 +0700

    BUTTN

--- MiniProject/src/main.cpp
+++ MiniProject/src/main.cpp
@@ -3,9 +3,13 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <WiFi.h>
-#define RED 26
-#define YELLOW 25
-#define GREEN 33
+#define LIGHT1 26
+#define LIGHT2 25
+#define LIGHT3 33
+//threshold 35
+#define SWITCH1 0 //T0
+#define SWITCH2 2 //T1
+#define SWITCH3 15 //T2
 void Connect_Wifi(const char* ssid,const char* password){
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -23,9 +27,9 @@ void main_task(void *param){//will run in paralelled
 
 void setup() {
     Serial.begin(115200);
-    pinMode(RED,OUTPUT);
-    pinMode(YELLOW,OUTPUT);
-    pinMode(GREEN,OUTPUT);
+    pinMode(LIGHT1,OUTPUT);
+    pinMode(LIGHT2,OUTPUT);
+    pinMode(LIGHT3,OUTPUT);
     Connect_Wifi("OPPO_KUY","oppopass");
     xTaskCreatePinnedToCore(main_task, "main_task", 10000, NULL, 1, NULL, 0);
     
