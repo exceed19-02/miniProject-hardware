28bdc57cf6115f23b995d6020d545a42e8128472
VeryHardBit <yanothaic15@gmail.com>
2023-02-08 13:49:01 +0700

    main.cpp

--- MiniProject/src/main.cpp
+++ MiniProject/src/main.cpp
@@ -3,8 +3,11 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <WiFi.h>
-void Connect_Wifi(){
-    WiFi.begin("OPPO_KUY", "oppopass");
+#define RED 26
+#define YELLOW 25
+#define GREEN 33
+void Connect_Wifi(const char* ssid,const char* password){
+    WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.println("Connecting to WiFi..");
@@ -12,11 +15,24 @@ void Connect_Wifi(){
     Serial.println("Connected to the WiFi network");
     Serial.println(WiFi.localIP());
 }
-void setup() {
-  // put your setup code here, to run once:
+
+void main_task(void *param){//will run in paralelled
+
 }
 
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(RED,OUTPUT);
+    pinMode(YELLOW,OUTPUT);
+    pinMode(GREEN,OUTPUT);
+    Connect_Wifi("OPPO_KUY","oppopass");
+    xTaskCreatePinnedToCore(main_task, "main_task", 10000, NULL, 1, NULL, 0);
+    
+}
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+
 }
