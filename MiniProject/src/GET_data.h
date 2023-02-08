3227f192ba47173a35cfb2f149faac5f90e39fb7
Crparichaya <parichaya.yan@ku.th>
2023-02-08 14:01:30 +0700

    update get

--- MiniProject/src/GET_data.h
+++ MiniProject/src/GET_data.h
@@ -5,9 +5,11 @@
 
 const String baseUrl = "http://group2.exceed19.online";
 
-int light_id; // from 1-3
-bool status;
-String mode;
+// from 1-3
+int light_id1, light_id2, light_id3;
+bool status1, status2, status3;
+String mode1, mode2, mode3;
+int brightness1, brightness2, brightness3;
 
 void GET_Data(int light_id){
     while(1){
@@ -23,16 +25,25 @@ void GET_Data(int light_id){
             String payload = http.getString();
             deserializeJson(doc, payload);
 
+            light_id1 = doc[0]["light_id1"];
+            light_id2 = doc[0]["light_id2"];
+            light_id3 = doc[0]["light_id3"];
+
+            status1 = doc[1]["status1"];
+            status2 = doc[1]["status2"];
+            status3 = doc[1]["status3"];
+
+            mode1 = (const char *)doc[2]["mode1"];
+            mode2 = (const char *)doc[2]["mode2"];
+            mode3 = (const char *)doc[2]["mode3"];
+
+
+            brightness1 = doc[3]["brightness1"];
+            brightness2 = doc[3]["brightnes2"];
+            brightness3 = doc[3]["brightnes3"];
             // *** write your code here ***
             // set up JSON
-            Serial.println();
-            Serial.println((int)doc["light_id"]);
-            Serial.println((bool)doc["status"]);
-            Serial.println((const char *)doc["mode"]);
-
-            light_id = (int)doc["light_id"];
-            status = (bool)doc["status"];
-            mode = (const char*)doc["mode"];
+            Serial.println(payload);
             
     
         }else{
